6c175598aad61648eda3797a1e0d15fe2d6f2df9
ema2159 <ema2159@gmail.com>
2019-12-24 13:32:32 -0600

    Add: Section pages

---
+++ src/components/WorkHistory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WorkHistory = () => {
+    return (
+        <div>
+          <h1>
+            Work
+          </h1>
+        </div>
+    );
+}
+
+export default WorkHistory;
