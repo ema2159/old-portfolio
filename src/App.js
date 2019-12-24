2b6cfbc743b3d0130bf23efe51d1605b12854106
ema2159 <ema2159@gmail.com>
2019-12-24 13:34:26 -0600

    Add: Containter under NavBar to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
+import Main from './components/main.js';
 import NavBar from './components/NavBar.js';
+import { Container } from '@material-ui/core';
 
 class App extends Component {
     render(){
 	return (
             <div>
               <NavBar></NavBar>
+              <Container>
+                <Main/>
+              </Container>
             </div>
         );
     }
