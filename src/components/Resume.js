3577c9fd518dc9306a1ba90bf653b4aed00a22ba
ema2159 <ema2159@gmail.com>
2019-12-25 17:23:35 -0600

    Mod: Centered Resume's file content using flex properties

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  ResumeRoot: {
+    display: 'flex',
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh'
+  },
+  h1Stuff: {
+    top: '100px',
+    left: '100%',
+  }
+}));
 
 const Resume = () => {
-    return (
-        <div>
-          <h1>
-            Resume
-          </h1>
-        </div>
-    );
-}
+  const classes = useStyles();
+  return (
+    <div className={classes.ResumeRoot}>
+      <h1 className={classes.h1Stuff}>Resume</h1>
+    </div>
+  );
+};
 
 export default Resume;
