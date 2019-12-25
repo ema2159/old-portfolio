e554feb86bbd93cd7a3dbf59928816004af12c81
ema2159 <ema2159@gmail.com>
2019-12-25 17:22:38 -0600

    Mod: Renamed main.js file to follow Pascal case convention

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import LandingPage from './LandingPage.js';
 import AboutMe from './AboutMe.js';
 import Resume from './Resume.js';
 import WorkHistory from './WorkHistory.js';
@@ -10,8 +9,7 @@ import Projects from './Projects.js';
 const Main = () => {
     return (
 	<Switch>
-          <Route exact path = '/' component={LandingPage}/>
-          <Route path = '/AboutMe' component={AboutMe}/>
+          <Route exact path = '/' component={AboutMe}/>
           <Route path = '/Resume' component={Resume}/>
           <Route path = '/Projects' component={Projects}/>
           <Route path = '/WorkHistory' component={WorkHistory}/>
