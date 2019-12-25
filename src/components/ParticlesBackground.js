ff6f21575c36c6aad6356258794f803c9337b26d
ema2159 <ema2159@gmail.com>
2019-12-25 17:43:17 -0600

    Mod: Configured particles background

--- src/components/ParticlesBackground.js
+++ src/components/ParticlesBackground.js
@@ -16,29 +16,56 @@ const ParticlesBackground = () => {
         params={{
           particles: {
             number: {
-              value: 20
+              value: 10,
+              density: { enable: true, value_area: 2000 }
+            },
+            color: { value: "#2e009d" },
+            shape: {
+              type: "circle",
+              stroke: { width: 0, color: "#000000" },
+              polygon: { nb_sides: 5 },
+              image: { src: "img/github.svg", width: 100, height: 100 }
+            },
+            opacity: {
+              value: 0.5,
+              random: false,
+              anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
             },
             size: {
-              value: 4
-            },
-            color: {
-              value: "#7d8eff"
+              value: 3,
+              random: true,
+              anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
             },
             line_linked: {
               enable: true,
-              distance: 150,
-              color: "#616585",
-              width: 2
+              distance: 250,
+              color: "#382398",
+              opacity: 0.4,
+              width: 1
+            },
+            move: {
+              enable: true,
+              speed: 6,
+              direction: "none",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: { enable: false, rotateX: 600, rotateY: 1200 }
             }
           },
           interactivity: {
+            detect_on: "canvas",
             events: {
-              onhover: {
-                enable: true,
-                mode: "grab"
-              }
+              onhover: { enable: true, mode: "grab" },
+              onclick: { enable: true, mode: "push" },
+              resize: true
+            },
+            modes: {
+              grab: { distance: 300, line_linked: { opacity: 1 } },
             }
-          }
+          },
+          retina_detect: true
         }}
       />
     </div>
