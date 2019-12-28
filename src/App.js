8978f32235bc3184ce877af67e502cf9e272eca6
ema2159 <ema2159@gmail.com>
2019-12-28 12:32:57 -0600

    Mod: Added gradient background to the main page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import { Container } from "@material-ui/core";
 class App extends Component {
   render() {
     return (
-      <div style={{ height: "100vh", overflow: "auto", background: "#edefff" }}>
+      <div style={{ height: "100vh",
+                    overflow: "auto",
+		    background: "#edefff",
+		    background: "-webkit-linear-gradient(to bottom, #FFFFFF, #edefff)",
+		    background: "linear-gradient(to bottom, #FFFFFF, #edefff)",
+                  }}>
 	<Main />
         <NavBar />
       </div>
