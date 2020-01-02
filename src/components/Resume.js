607f3d8861c886940661022f9d86770ac34fd947
ema2159 <ema2159@gmail.com>
2020-01-01 22:04:40 -0600

    Mod: Centered and scaled resume in Resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import PDF from "react-pdf-js";
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+}));
+
 const Resume = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <PDF
+        scale={2}
 	file="resume.pdf"
         page={1}
       />
