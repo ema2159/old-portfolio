a898944575142d22de473f4f7bba21acfd16263b
ema2159 <ema2159@gmail.com>
2020-01-20 20:43:19 -0600

    Mod: Format Main.js file

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import AboutMe from './AboutMe.js';
-import Resume from './Resume.js';
-import WorkHistory from './WorkHistory.js';
-import Projects from './Projects.js';
+import AboutMe from "./AboutMe.js";
+import Resume from "./Resume.js";
+import WorkHistory from "./WorkHistory.js";
+import Projects from "./Projects.js";
 
 const Main = () => {
-    return (
-	<Switch>
-          <Route exact path = '/' component={AboutMe}/>
-          <Route path = '/Resume' component={Resume}/>
-          <Route path = '/Projects' component={Projects}/>
-          <Route path = '/WorkHistory' component={WorkHistory}/>
-	</Switch>
-    );
-}
+  return (
+    <Switch>
+      <Route exact path="/" component={AboutMe} />
+      <Route path="/Resume" component={Resume} />
+      <Route path="/Projects" component={Projects} />
+      <Route path="/WorkHistory" component={WorkHistory} />
+    </Switch>
+  );
+};
 
 export default Main;
