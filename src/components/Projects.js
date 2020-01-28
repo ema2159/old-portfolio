9f2a35126c7bb53575f77d795757b02759048b72
ema2159 <ema2159@gmail.com>
2020-01-28 10:23:00 -0600

    Add: Enter animations to Projects page component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,8 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import Radio from "@material-ui/core/Radio";
 import Paper from "@material-ui/core/Paper";
+import styled, { keyframes } from "styled-components";
+import { fadeInRight } from "react-animations";
 
 // Import file with projects info
 const projectsJSON = require("../data/projects.json");
@@ -16,7 +18,7 @@ const projectsJSON = require("../data/projects.json");
 const emacsImage = require("../img/emacs.png");
 const reactImage = require("../img/react.png");
 
-// Create styles
+// Styles
 const useStyles = makeStyles(theme => ({
   root: {
     position: "relative",
@@ -28,13 +30,14 @@ const useStyles = makeStyles(theme => ({
     overflow: "auto"
   },
   grid: {
-    marginTop: "6%",
+    marginTop: "12vh",
     display: "flex",
     justifyContent: "center",
     alignItems: "flex-start"
   },
   innerGrid: {
-    width: "50vw"
+    width: "50vw",
+    justifyContent: "left",
   },
   paper: {
     height: "30vh",
@@ -58,7 +61,8 @@ const useStyles = makeStyles(theme => ({
   },
   title: {
     color: "#F8F8F8",
-    marginLeft: "2%"
+    marginLeft: "2%",
+    fontSize: "calc(10px + 1vw)"
   },
   body: {
     height: "70%",
@@ -79,8 +83,9 @@ const useStyles = makeStyles(theme => ({
   image: {
     height: "auto",
     width: "auto",
-    maxWidth: "80%",
-    maxHeight: "80%"
+    marginLeft: "10px",
+    maxWidth: "50%",
+    maxHeight: "50%"
   },
   headerDivider: {
     borderTop: "2px solid #264653",
@@ -92,6 +97,8 @@ const useStyles = makeStyles(theme => ({
     color: "#264653",
     borderColor: "#264653",
     width: "5vw",
+    height: "4vh",
+    fontSize: "calc(10px + 0.3vw)",
     "&:hover": {
       borderColor: "#5E81AC",
       background: "#5E81AC"
@@ -99,47 +106,55 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-// Project objects
+// Animations
+const ProjectsAnimation = styled.div`
+  --time: ${props => props.time};
+  animation: var(--time) ${keyframes(fadeInRight)};
+`;
+
+// Main component
 const Projects = () => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Grid className={classes.grid} item xs={12}>
-        <Grid
-          container
-          className={classes.innerGrid}
-          justify="center"
-          spacing={1}
-        >
-          {projectsJSON.map((project, index) => (
-            <Grid key={project.id} item>
-              <Paper className={classes.paper}>
-                <div className={classes.header}>
-                  <img className={classes.image} src={eval(project.image)} />
-                  <h1 className={classes.title}>{project.name}</h1>
-                </div>
-                <div className={classes.body}>
-                  <div>
-                    <Typography variant="body1" className={classes.text}>
-                      {project.description}
-                    </Typography>
-                    <hr className={classes.headerDivider} />
-                  </div>
-                  <div>
-                    <Button
-                      className={classes.githubButton}
-                      variant="outlined"
-                      color="primary"
-                      size="medium"
-                      href={project.github}
-                      target="_blank"
-                    >
-                      Github
-                    </Button>
-                  </div>
-                </div>
-              </Paper>
+	<Grid
+	  container
+	  className={classes.innerGrid}
+	  justify="center"
+	  spacing={2}
+	>
+	  {projectsJSON.map((project, index) => (
+	    <Grid key={project.id} item>
+	    <ProjectsAnimation time={index < 8 ? 0.5+0.2*index+"s" : index%2 ? "2s" :"1.8s"}>
+		<Paper className={classes.paper}>
+		  <div className={classes.header}>
+		    <img className={classes.image} src={eval(project.image)} />
+		    <h1 className={classes.title}>{project.name}</h1>
+		  </div>
+		  <div className={classes.body}>
+		    <div>
+		      <Typography variant="body1" className={classes.text}>
+			{project.description}
+		      </Typography>
+		      <hr className={classes.headerDivider} />
+		    </div>
+		    <div>
+		      <Button
+			className={classes.githubButton}
+			variant="outlined"
+			color="primary"
+			size="medium"
+			href={project.github}
+			target="_blank"
+		      >
+			Github
+		      </Button>
+		    </div>
+		  </div>
+		</Paper>
+              </ProjectsAnimation>
             </Grid>
           ))}
         </Grid>
