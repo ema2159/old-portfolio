29f2214329a0ca6f69f865edb14433e16b3a4e0c
ema2159 <ema2159@gmail.com>
2020-01-27 23:19:30 -0600

    Mod: Improved Projects component colors

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -44,20 +44,20 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
     flexFlow: "column",
     alignItems: "center",
-    background: "#172D35"
+    background: "#F8F8F8"
   },
   header: {
     position: "relative",
     width: "100%",
     height: "30%",
-    background: "#F8F8F8",
+    background: "#172D35",
     borderTopLeftRadius: "10px",
     borderTopRightRadius: "10px",
     display: "flex",
     alignItems: "center"
   },
   title: {
-    color: "#264653",
+    color: "#F8F8F8",
     marginLeft: "2%"
   },
   body: {
@@ -65,37 +65,36 @@ const useStyles = makeStyles(theme => ({
     textAlign: "center",
     display: "flex",
     flexFlow: "column",
-    justifyContent: "space-around",
+    justifyContent: "space-around"
   },
   text: {
-    fontFamily: "Open Sans",
-    fontStyle: "bold",
     textAlign: "justify",
     marginLeft: "5%",
     marginRight: "5%",
     marginTop: "5%",
-    color: "#E0E0E0",
-    fontSize: "calc(10px + 0.3vw)",
+    marginBottom: "5%",
+    color: "#264653",
+    fontSize: "calc(10px + 0.3vw)"
   },
   image: {
     height: "auto",
     width: "auto",
     maxWidth: "80%",
-    maxHeight: "80%",
+    maxHeight: "80%"
   },
   headerDivider: {
-    borderTop: "2px dotted #E0E0E0",
-    color: "#172D35",
+    borderTop: "2px solid #264653",
+    color: "#F8F8F8",
     marginTop: "10px",
-    width: "95%",
+    width: "70%"
   },
   githubButton: {
-    color: "#E0E0E0",
-    borderColor: "#E0E0E0",
+    color: "#264653",
+    borderColor: "#264653",
     width: "5vw",
     "&:hover": {
       borderColor: "#5E81AC",
-      background: "#5E81AC",
+      background: "#5E81AC"
     }
   }
 }));
@@ -107,32 +106,38 @@ const Projects = () => {
   return (
     <div className={classes.root}>
       <Grid className={classes.grid} item xs={12}>
-	<Grid container className={classes.innerGrid} justify="center" spacing={1}>
-	  {projectsJSON.map(project => (
-	    <Grid key={project.id} item>
-	      <Paper className={classes.paper}>
-		<div className={classes.header}>
-		  <img className={classes.image} src={eval(project.image)} />
-		  <h1 className={classes.title}>{project.name}</h1>
-		</div>
-		<div className={classes.body}>
-		  <div>
-		    <Typography variant="body1" className={classes.text}>
-		      {project.description}
-		    </Typography>
-		    <hr className={classes.headerDivider}/>
-		  </div>
-		  <div>
-		    <Button
-		      className={classes.githubButton}
-		      variant="outlined"
-		      color="primary"
-		      size="medium"
-		      href={project.github}
-		      target="_blank" >
-		      Github
-		    </Button>
-		  </div>
+        <Grid
+          container
+          className={classes.innerGrid}
+          justify="center"
+          spacing={1}
+        >
+          {projectsJSON.map((project, index) => (
+            <Grid key={project.id} item>
+              <Paper className={classes.paper}>
+                <div className={classes.header}>
+                  <img className={classes.image} src={eval(project.image)} />
+                  <h1 className={classes.title}>{project.name}</h1>
+                </div>
+                <div className={classes.body}>
+                  <div>
+                    <Typography variant="body1" className={classes.text}>
+                      {project.description}
+                    </Typography>
+                    <hr className={classes.headerDivider} />
+                  </div>
+                  <div>
+                    <Button
+                      className={classes.githubButton}
+                      variant="outlined"
+                      color="primary"
+                      size="medium"
+                      href={project.github}
+                      target="_blank"
+                    >
+                      Github
+                    </Button>
+                  </div>
                 </div>
               </Paper>
             </Grid>
