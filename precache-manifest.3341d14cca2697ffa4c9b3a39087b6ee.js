208c221514b8b10478236f72edbeb27e908a6414
ema2159 <ema2159@gmail.com>
2020-01-29 10:32:07 -0600

    Updates

---
+++ precache-manifest.3341d14cca2697ffa4c9b3a39087b6ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdeb3afd0586e6a6d21f3f03e4f88cad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ac430c80a37cd7af2d7",
+    "url": "/static/css/main.c80ad70b.chunk.css"
+  },
+  {
+    "revision": "4c9360355aa49fdac732",
+    "url": "/static/js/2.db9e4b3d.chunk.js"
+  },
+  {
+    "revision": "1d662a8785a24289b5e22904e818c230",
+    "url": "/static/js/2.db9e4b3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ac430c80a37cd7af2d7",
+    "url": "/static/js/main.f9b62dec.chunk.js"
+  },
+  {
+    "revision": "4b1915131297c5248b6c",
+    "url": "/static/js/runtime-main.cf831cec.js"
+  },
+  {
+    "revision": "6fb5f1ceb9e2bfa717b569c4ce92d5bf",
+    "url": "/static/media/emacs.6fb5f1ce.png"
+  },
+  {
+    "revision": "d5e08cd1bc2797eb9f6bcdf90dea6806",
+    "url": "/static/media/react.d5e08cd1.png"
+  },
+  {
+    "revision": "bfd5a0c0724703cadda8393a692b7df6",
+    "url": "/static/media/resume.bfd5a0c0.pdf"
+  }
+]);
