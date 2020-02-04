06f82d3aa12ddbb68cc1fec479e980628926febc
ema2159 <ema2159@gmail.com>
2020-02-04 10:11:15 -0600

    Fix: Remove references to deleted Resume component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import AboutMe from "./AboutMe.js";
-import Resume from "./Resume.js";
 import Contact from "./Contact.js";
 import Projects from "./Projects.js";
 
@@ -11,7 +10,6 @@ const Main = () => {
   return (
     <Switch>
       <Route exact path="/" component={AboutMe} />
-      <Route path="/Resume" component={Resume} />
       <Route path="/Projects" component={Projects} />
       <Route path="/Contact" component={Contact} />
     </Switch>
